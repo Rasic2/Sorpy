b662afc9d943b3862b7befcff94b88d4ad68081f
Rasic2 <1051987201@qq.com>
2021-09-08 22:08:44 +0800

    debug ML_model

--- common/structure.py
+++ common/structure.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 import numpy as np
 from collections import defaultdict
 
@@ -25,7 +26,10 @@ class Molecule:
 
     @property
     def frac_coords(self):
-        return None
+        frac_vector = self.coords[1] -self.coords[0]
+        self.coords[1] = np.where(frac_vector > 0.5, self.coords[1] - 1, self.coords[1])
+        self.coords[1] = np.where(frac_vector < -0.5, self.coords[1] + 1, self.coords[1])
+        return self.coords
 
     @property
     def cart_coords(self):
@@ -41,9 +45,9 @@ class Molecule:
 
     @property
     def theta(self):
-        Axisz=np.array([0, 0, 1])
-        return math.degrees(math.acos(np.dot(Axisz, self.vector)/ \
-                                      (np.linalg.norm(Axisz, ord=1) * np.linalg.norm(self.vector, ord=1))))
+        z = self.vector[2]
+        r = self.bond_length
+        return math.degrees(math.atan2(math.sqrt(r**2-z**2), z))
 
     @property
     def phi(self):
@@ -54,40 +58,74 @@ class Molecule:
 
 class Latt:
 
-    def __init__(self, string):
-        self.string = string
-        assert isinstance(self.string, list)
-        self.matrix = np.array([[float(ii) for ii in item.split()] for item in self.string])
+    def __init__(self, matrix):
+        """
+        TODO inverse of matrix
+        :param string:
+        """
+        assert type(matrix) == np.ndarray
+
+        self.matrix = matrix
+
+    @property
+    def inverse(self):
+        return np.linalg.inv(self.matrix)
+
+    @staticmethod
+    def read_from_string(string):
+        """
+        :param string:      POSCAR文件中的3行Lattice矢量
+        :return:
+        """
+
+        assert isinstance(string, list)
+        assert len(string) == 3
+
+        matrix = np.array([[float(ii) for ii in item.split()] for item in string])
+        return Latt(matrix)
 
 class POSCAR:
 
-    def __init__(self, fname):
+    def __init__(self, fname, action="r"):
         self.fname = fname
-        self.__load()
+        self.action = action
+        self.__load() if self.action == "r" else None
 
     def __load(self):
-        with open(self.fname, "r") as f:
+        with open(self.fname, self.action) as f:
             self.cfg = f.readlines()
         self.system = self.cfg[0].rstrip()
         self.factor = self.cfg[1].rstrip()
-        self.latt = Latt(self.cfg[2:5]).matrix
-        self.element = [(name, int(count)) for name, count in zip(self.cfg[5].split(), self.cfg[6].split())]
+        self.latt = Latt.read_from_string(self.cfg[2:5])
+        self.elements = [(name, int(count)) for name, count in zip(self.cfg[5].split(), self.cfg[6].split())]
         self.selective = self.cfg[7].lower()[0] == "s"
         self.coor_type = self.cfg[8].rstrip()
         assert self.coor_type.lower()[0] == "d"
         self.frac_coors = self._frac_coors()
         self.cart_coors = self._cart_coors()
+        self.TF = self._TF()
 
     @property
     def ele_count(self):
-        return sum(n for _, n in self.element)
+        return sum(n for _, n in self.elements)
 
     def _frac_coors(self):
         if self.selective:
             return np.array([[float(ii) for ii in item.split()[:3]] for item in self.cfg[9:9+self.ele_count]])
+        else:
+            return None
 
     def _cart_coors(self):
-        return np.dot(self._frac_coors(), self.latt)
+        if self.frac_coors is not None:
+            return np.dot(self.frac_coors, self.latt.matrix)
+        else:
+            return None
+
+    def _TF(self):
+        if self.selective:
+            return np.array([item.split()[3:6] for item in self.cfg[9:9+self.ele_count]])
+        else:
+            return None
 
     def nearest_neighbour_table(self):
 
@@ -108,8 +146,80 @@ class POSCAR:
         setattr(self, "NNT", sorted_NNT)
         return sorted_NNT
 
+    @staticmethod
+    def mcoord_to_coord(array, latt, anchor, intercoor_index):
+
+        assert type(array) == np.ndarray
+        assert type(latt) == Latt
+
+        total_index = list(range(len(array)))
+        frac_index = list(set(total_index).difference(set(intercoor_index)))
+        anchor_cart_coor = np.dot(array[anchor], latt.matrix)
+        intercoor = array[intercoor_index] * [1, 180, 360] + [1.142, 0, 0] # TODO setting.yaml modify
+        intercoor = intercoor.reshape(3)
+        r, theta, phi = intercoor[0], math.radians(intercoor[1]), math.radians(intercoor[2])
+        x = r * math.sin(theta) * math.cos(phi)
+        y = r * math.sin(theta) * math.sin(phi)
+        z = r * math.cos(theta)
+        mol_cart_coor = anchor_cart_coor + [x, y, z]
+        mol_frac_coor = np.dot(mol_cart_coor, latt.inverse).reshape((1,3))
+        return np.concatenate((array[frac_index], mol_frac_coor), axis=0)
+
+    def write(self, template, coor):
+
+        assert type(template) == POSCAR
+        assert self.action == "w"
+
+        with open(self.fname, self.action) as f:
+            f.writelines(template.cfg[:9])
+            for ii, jj in zip(coor, template.TF):
+                item = f"{ii[0]:.6f} {ii[1]:.6f} {ii[2]:.6f} {jj[0]} {jj[1]} {jj[2]} \n"
+                f.write(item)
+
+    def align(self, template):
+        indicator = 0 # 第二次对齐的原子
+        repeat = 2 # supercell
+        slab_coor = self.frac_coors[:36]
+        mol_coor = self.frac_coors[36:38]
+        model_region = np.array([1/repeat, 1/repeat])
+        mass_center = np.mean(mol_coor, axis=0)[:2]
+        search_vector = np.arange(-1, 1+1/repeat, 1/repeat)
+        search_matrix = itertools.product(search_vector, search_vector)
+        for ii in search_matrix:
+            if 0 <= (mass_center + ii)[0] <= model_region[0] and 0 <= (mass_center + ii)[1] <= model_region[1]:
+                vector_m = [ii[0], ii[1], 0]
+                break
+        coor_m = mol_coor + vector_m
+        coor_first = np.concatenate((slab_coor, coor_m), axis=0)
+
+        for ii, jj in zip(self.elements, template.elements):
+            if ii[0] != jj[0] or ii[1] != jj[1]:
+                raise NotImplementedError # TODO 3x3 or len(atoms) 不相等
+
+        self.frac_coors = coor_first
+        self.cart_coors = self._cart_coors() # Reset the cart_coords
+
+        NNT = defaultdict(list)
+        for ii in range(self.ele_count):
+            for jj in range(template.ele_count):
+                dis = distance(self.cart_coors[ii], self.cart_coors[jj])
+                NNT[ii].append((jj, dis))
+
+        sorted_NNT = defaultdict(list)
+        for key, value in NNT.items():
+            sorted_NNT[key] = sorted(value, key=lambda x: x[1])
+
+        min_NNT = []
+        for key, value in sorted_NNT.items():
+            min_NNT.append((key, value[0][0], value[0][1]))
+
+        for ii, jj, kk in sorted(min_NNT, key=lambda x: x[2]):
+            print(ii, jj)
+        #    if mass_center + model_region * i
+        #print(model_region)
 
 if __name__ == "__main__":
+
     for ii in range(50):
         print(f"POSCAR_ML_{ii+1}")
         p = POSCAR(f"../test/ML-2/POSCAR_ML_{ii+1}")
--- generate_test.py
+++ generate_test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+from common.structure import POSCAR
+
+template = POSCAR("input/POSCAR_1-1", action="r")
+POSCAR("test/ori/POSCAR_ori_3", action="r").align(template)
+exit()
+
 import random
 import numpy as np
 from keras.models import load_model
@@ -11,24 +17,6 @@ from load_yaml import ParameterManager
 from generate_adsorbate import surface_cleave, random_molecule_getter
 
 
-def array_to_POSCAR(array, POSCAR_t: str, fname):
-    with open(POSCAR_t, "r") as f:
-        cfg = f.readlines()
-    head = cfg[:7]
-    element_num = sum([int(ii) for ii in head[6].split()])
-    selective_flag = cfg[7][0].lower() == "s"
-    if selective_flag:
-        head += cfg[7:9]
-        TF = [item.split()[3:6] for item in cfg[9:9+element_num]]
-    else:
-        raise NotImplementedError("Selective Dynamics = False is not implemented")
-
-    with open(fname, "w") as f:
-        f.writelines(head)
-        for ii, jj in zip(array, TF):
-            item = f"{ii[0]:.6f} {ii[1]:.6f} {ii[2]:.6f} {jj[0]} {jj[1]} {jj[2]} \n"
-            f.write(item)
-
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # 屏蔽TF日志输出
 
@@ -37,8 +25,8 @@ Test_DIR = os.path.join(current_dir, "test")
 if not os.path.exists(Test_DIR):
     os.mkdir(Test_DIR)
 
-ori_test_dir = os.path.join(Test_DIR, "ori")
-ML_test_dir = os.path.join(Test_DIR, "ML-2")
+ori_test_dir = os.path.join(Test_DIR, "ori-2")
+ML_test_dir = os.path.join(Test_DIR, "ML-2_1")
 
 PM = ParameterManager("test_111.yaml")
 asf_CeO2_surf = surface_cleave(PM.MillerIndex)
@@ -49,7 +37,7 @@ if not os.path.exists(ori_test_dir):
 
 for ii in range(PM.TestNum):
     molecule = random_molecule_getter()
-    j = np.random.random((1, 2))
+    j = np.random.random((1, 2)) / 2
     Mat2 = np.dot(j, latt)
     shiftz = 2 * random.random() - 1
     CO_ads = asf_CeO2_surf.add_adsorbate(molecule, [Mat2[0, 0], Mat2[0, 1], PM.z_height + shiftz])
@@ -58,16 +46,23 @@ for ii in range(PM.TestNum):
     p = Poscar(CO_ads)
     #p.write_file(f"{ori_test_dir}/POSCAR_ori_{ii+1}")
 
-test_ori_DM = DirManager("test/ori", "POSCAR", "37-38")
-test_ori_coor = test_ori_DM.coords
+
+
+#exit()
+test_ori_DM = DirManager("test/ori-2", "POSCAR", "37-38")
+test_ori_coor = test_ori_DM.mcoords
 test_ori_coor = test_ori_coor.reshape((test_ori_coor.shape[0], 38*3))
 
-model = load_model("CeO2_111_CO.h5")
+model = load_model("CeO2_111_CO_test.h5")
 test_ML_coor = model.predict(test_ori_coor)
 test_ML_coor = test_ML_coor.reshape((test_ML_coor.shape[0], 38, 3))
 
 if not os.path.exists(ML_test_dir):
     os.mkdir(ML_test_dir)
 
+template = POSCAR("test/ori/POSCAR_ori_1", action="r")
+lattice = template.latt
+
 for ii, item in enumerate(test_ML_coor):
-    array_to_POSCAR(item, f"{ori_test_dir}/POSCAR_ori_1", f"{ML_test_dir}/POSCAR_ML_{ii+1}")
+    coor = POSCAR.mcoord_to_coord(item, lattice, anchor=36, intercoor_index=[37])
+    POSCAR(f"{ML_test_dir}/POSCAR_ML_{ii+1}", action="w").write(template, coor)

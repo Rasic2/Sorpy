fc4f3f565e09987c2ff42a78040566e3656727ca
Rasic2 <1051987201@qq.com>
2021-09-20 21:20:05 +0800

    Try the new ML predict method, xdat-o

--- train_model.py
+++ train_model.py
@@ -9,8 +9,8 @@ from common.model import Model, Ploter
 from common.logger import logger, root_dir
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-input_dir = Path(root_dir) / "train_set" / "xinput-m"
-output_dir = Path(root_dir) / "train_set" / "xoutput-m"
+input_dir = Path(root_dir) / "train_set" / "xinput-o"
+output_dir = Path(root_dir) / "train_set" / "xoutput-o"
 
 if __name__ == "__main__":
 
@@ -30,25 +30,27 @@ if __name__ == "__main__":
     if data_load == "c":
         logger.info("Calculate the mcoords.")
         data_input, data_output = copy.deepcopy(input_dm.mcoords), copy.deepcopy(output_dm.mcoords)
-        with h5py.File(Path(root_dir)/"results/data_train.h5", "w") as hf:
+        with h5py.File(Path(root_dir)/"results/data_train-xdat-o.h5", "w") as hf:
             hf.create_dataset("data_input", data=data_input)
             hf.create_dataset("data_output", data=data_output)
     elif data_load == "f":
         logger.info("Load data from the .h5 file.")
-        with h5py.File(Path(root_dir)/"results/data_train.h5", "r") as hf:
+        with h5py.File(Path(root_dir)/"results/data_train-xdat-o.h5", "r") as hf:
             data_input = hf["data_input"][:]
             data_output = hf["data_output"][:]
     else:
         raise TypeError("Please indicate the load method of model train data, <'c' or 'f'>")
 
-    from keras import models, layers
+    from keras import models, layers, regularizers
     model = models.Sequential()
-    model.add(layers.Dense(1024, activation='relu', input_shape=(38 * 3,)))
+    model.add(layers.Dense(128, activation='relu', input_shape=(38 * 3,)))
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(128, activation='relu'))
     model.add(layers.Dense(114))
     model.compile(loss='mse', optimizer='rmsprop', metrics=['mae'])
 
     train_model = Model(model, data_input, data_output, normalization="mcoord", expand=kargs['expand'])
-    history = train_model("hold out", mname=Path(root_dir)/"results/CeO2_111_CO_test.h5", epochs=3)
+    history = train_model("hold out", mname=Path(root_dir)/"results/CeO2_111_CO_xdat_o.h5", epochs=3)
 
     p = Ploter(history)
     p.plot(fname=Path(root_dir)/"results/CeO2_111_history.svg")
